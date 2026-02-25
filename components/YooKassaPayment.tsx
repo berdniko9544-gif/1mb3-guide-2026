467b5cf293aacfe61290e6217bb6a1d8c9f3d322
berdniko9544-gif <berdniko9544@gmail.com>
2026-02-25 22:33:14 +0300

    Fix payment button: use custom styled button only, hide YooKassa default

--- components/YooKassaPayment.tsx
+++ components/YooKassaPayment.tsx
@@ -3,31 +3,38 @@
 import { Button } from './Button';
 
 export function YooKassaPayment() {
-  return (
-    <form
-      action="https://yookassa.ru/integration/simplepay/payment"
-      method="post"
-      acceptCharset="utf-8"
-      className="w-full"
-    >
-      {/* Hidden inputs for YooKassa configuration */}
-      <input
-        name="shopSuccessURL"
-        type="hidden"
-        value="https://disk.yandex.ru/i/wg1CGVedSqWvWQ"
-      />
-      <input
-        name="shopFailURL"
-        type="hidden"
-        value="https://t.me/Inside1mb3"
-      />
-      <input name="customerNumber" type="hidden" value="Гайд по нейросетям" />
-      <input name="sum" type="hidden" value="1990" />
-      <input name="shopId" type="hidden" value="1250536" />
+  const handlePayment = () => {
+    // Создаём форму программно и отправляем
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = 'https://yookassa.ru/integration/simplepay/payment';
+    form.acceptCharset = 'utf-8';
+    
+    const fields = {
+      shopSuccessURL: 'https://disk.yandex.ru/i/wg1CGVedSqWvWQ',
+      shopFailURL: 'https://t.me/Inside1mb3',
+      customerNumber: 'Гайд по нейросетям',
+      sum: '1990',
+      shopId: '1250536'
+    };
+    
+    Object.entries(fields).forEach(([name, value]) => {
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = name;
+      input.value = value;
+      form.appendChild(input);
+    });
+    
+    document.body.appendChild(form);
+    form.submit();
+  };
 
+  return (
+    <div className="w-full">
       {/* Custom styled button */}
       <Button
-        type="submit"
+        onClick={handlePayment}
         size="lg"
         className="w-full text-xl py-5 md:py-4 min-h-[64px] md:min-h-[56px] shadow-[0_12px_48px_rgba(120,140,255,.4)] hover:shadow-[0_16px_64px_rgba(120,140,255,.5)]"
         magnetic={false}
@@ -50,6 +57,6 @@ export function YooKassaPayment() {
           className="opacity-60"
         />
       </div>
-    </form>
+    </div>
   );
 }
